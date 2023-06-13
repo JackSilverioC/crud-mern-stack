5de1a91fa326dfbcccadaef922397ef5e49277f1
Jack Silverio <silveriocieza.jackanibal@gmail.com>
2023-06-13 18:55:19 -0500

    first commit

---
+++ src/config.js
@@ -0,0 +1,10 @@
+import { config } from "dotenv";
+
+config();
+
+export const TOKEN_SECRET = process.env.TOKEN_SECRET || "some secret key";
+
+export const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/merndb";
+
+export const PORT = process.env.PORT || 3000;
---
+++ src/schemas/auth.schema.js
@@ -0,0 +1,38 @@
+import { z } from "zod";
+
+export const registerSchema = z.object({
+  username: z.string({
+    required_error: "Username is required"
+  }),
+  email: z
+    .string({
+      required_error: "Email is required"
+    })
+    .email({
+      message: "El email no es válido"
+    }),
+  password: z
+    .string({
+      required_error: "Password is required"
+    })
+    .min(6, {
+      message: "La contraseña debe tener al menos 6 caracteres"
+    })
+});
+
+export const loginSchema = z.object({
+  email: z
+    .string({
+      required_error: "Email is required"
+    })
+    .email({
+      message: "El email no es válido"
+    }),
+  password: z
+    .string({
+      required_error: "Password is required"
+    })
+    .min(6, {
+      message: "La contraseña debe tener al menos 6 caracteres"
+    })
+});
