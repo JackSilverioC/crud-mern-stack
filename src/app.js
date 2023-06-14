adfd550d4d03f502e4461860f6270c09ebe52c8a
Jack Silverio <silveriocieza.jackanibal@gmail.com>
2023-06-14 09:51:35 -0500

    fixing

--- src/app.js
+++ src/app.js
@@ -27,6 +27,16 @@ app.use(cookieParser());
 app.use("/api", authRoutes);
 app.use("/api", tasksRoutes);
 
-app.use(express.static(join(__dirname, "../client/dist")));
+if (process.env.NODE_ENV === "production") {
+  const path = await import("path");
+  app.use(express.static("client/dist"));
+
+  app.get("*", (req, res) => {
+    console.log(path.resolve("client", "dist", "index.html"));
+    res.sendFile(path.resolve("client", "dist", "index.html"));
+  });
+}
+
+// app.use(express.static(join(__dirname, "../client/dist")));
 
 export default app;
