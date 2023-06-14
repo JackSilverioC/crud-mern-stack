fe05152ec7b45e70672a0343150f1c6eacc83a16
Jack Silverio <silveriocieza.jackanibal@gmail.com>
2023-06-13 19:08:28 -0500

    Fixing

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://crud-mern-stack-production-846f.up.railway.app//api",
+  baseURL: "https://crud-mern-stack-production-846f.up.railway.app/api",
   withCredentials: true
 });
 
